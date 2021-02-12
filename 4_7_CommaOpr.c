b8be65cd2bbab33a7d8838eacec4e6157b8423db
anujprj143 <dharmrp.143@gmail.com>
2021-02-12 15:55:48 +0530

    A simple program to understand Comma Operator in C

---
+++ 4_7_CommaOpr.c
@@ -0,0 +1,11 @@
+/*P4.7 Program to understand the use of comma operator*/
+#include<stdio.h>
+int main(void)
+{
+	int a,b,c,sum;
+	sum = (a=8,b=7,c=9,a+b+c); 
+		
+	printf("Sum=%d\n",sum); 
+	//Guess the output
+	return 0;
+}
