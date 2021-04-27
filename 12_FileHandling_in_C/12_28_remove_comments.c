488e92900d2f0ff47a23714b3b9949c523c31ea7
anujprj143 <dharmrp.143@gmail.com>
2021-04-27 21:36:57 +0530

    Program to copy a file into another file after removing the comments

---
+++ 12_FileHandling_in_C/12_28_remove_comments.c
@@ -0,0 +1,38 @@
+/*12.28. Program to copy a file into another file after removing the comments*/
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	FILE *fp1,*fp2;
+	char name[50];
+	int c1,c2,found='n';
+	printf("Enter the file name : ");
+	scanf("%s",name);
+
+	if((fp1=fopen(name,"r"))==NULL)
+	{
+		printf("Error in opening file\n");
+		exit(1);
+	}
+	fp2 = fopen("new1.c","w");
+	c1 = fgetc(fp1);
+	c2 = fgetc(fp1);
+
+	while(c2!=EOF)
+	{
+		if(c1=='/' && c2=='*')
+		   found = 'y';
+		if(found=='n')
+		   fputc(c1,fp2);
+		if(c1=='*' && c2=='/')
+		{
+			found = 'n';
+			c2 = fgetc(fp1);
+		}
+		c1 = c2;
+		c2 = fgetc(fp1);
+	}
+	fclose(fp1);
+	fclose(fp2);
+	return 0;
+}
