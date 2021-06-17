7212d18ad0c7acff267053825c121711ba96814d
anujprj143 <dharmrp.143@gmail.com>
2021-06-17 22:21:39 +0530

    Program to understand the use of local static variables.

---
+++ Chapter15/15_06_static_variable.c
@@ -0,0 +1,17 @@
+/*15.06. Program to understand the use of local static variables*/
+#include<stdio.h>
+void func(void);
+int main(void)
+{
+	func();
+	func();
+	func();	
+	return 0;
+}
+void func(void)
+{
+	static int x=2, y=5;
+	printf("x=%d, y=%d\n",x,y);
+	x++;   
+	y++;
+}
