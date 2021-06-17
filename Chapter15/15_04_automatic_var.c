fa27d4ea5b631037fbd40b00928a6b5c7333eaa8
anujprj143 <dharmrp.143@gmail.com>
2021-06-17 22:09:24 +0530

    Program to understand automatic variables.

---
+++ Chapter15/15_04_automatic_var.c
@@ -0,0 +1,17 @@
+/*15.04. Program to understand automatic variables*/
+#include<stdio.h>
+int main(void)
+{
+	int x=3;
+	printf("%d\t",x);
+	{
+		int x=10;
+		printf("%d\t",x);
+	}
+	{
+		int x=26;
+		printf("%d\t",x);
+	}
+	printf("%d\n",x);
+	return 0;
+}
