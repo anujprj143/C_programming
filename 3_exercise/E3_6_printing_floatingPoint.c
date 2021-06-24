e106db16eaab9369b456ec1cdf95e66f3f2952ea
anujprj143 <dharmrp.143@gmail.com>
2021-06-24 21:24:47 +0530

    Formatting the length of float values.

---
+++ 3_exercise/E3_6_printing_floatingPoint.c
@@ -0,0 +1,10 @@
+/*E3.6. Formatting the length of float values. */
+#include<stdio.h>
+int main(void)
+{
+	float b=123.1265;
+	printf("%f\t",b);
+	printf("%.2f\t",b);
+	printf("%.3f\n",b);
+	return 0;
+}
